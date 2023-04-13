665c65f508433eb06be85d890ff805262d95cfa5
DamFil <damjan.filipovic.21@um.edu.mt>
2023-04-13 11:18:58 +0200

    Declared and defined a class for handling the input file stream

---
+++ FileStreamHandler.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class FileHandler {
+    private:
+        string path;
+        fstream file;
+        string buffer;
+        int inputpos;
+    public:
+        FileHandler() : inputpos(0) {
+            cout << "Specify the path of the file to compile: ";
+            cin >> path;
+            file.open(path, ifstream::in);
+            if (!file.is_open()) {
+                cout << "Error: Cannot open file at: " + path << endl;
+                buffer = "";
+            } 
+            else 
+                buffer = { (std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()) };
+        }
+
+        FileHandler(string p) : path(p) , inputpos(0) {
+            file.open(path, ifstream::in);
+
+            if (!file.is_open()) {
+                cout << "Error: Cannot open file at: " + path << endl;
+                buffer = "";
+            }
+            else 
+                buffer = { (std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()) };
+        }
+
+    char NextChar() {
+        char c = buffer.at(inputpos);
+        if (c != EOF) 
+            ++inputpos;
+        return c;
+    }
+
+    void RollBack() {
+        --inputpos;
+    }
+};
