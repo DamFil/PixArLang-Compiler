ca103b66ad8962cdcbddcf782b18ff7bebd92cac
DamFil <damjan.filipovic.21@um.edu.mt>
2023-05-11 10:25:36 +0200

    Renamed the peek function in scanner as well as introduced the rollback function

--- Scanner/Scanner.cpp
+++ Scanner/Scanner.cpp
@@ -1,189 +1,225 @@
 #include "Scanner.h"
 
-//constructors
-Scanner::Scanner() : file(FileHandler(path)), current(0) {
+// constructors
+Scanner::Scanner() : file(FileHandler(path)), current(0)
+{
     cout << "Specify the path to the program: ";
     cin >> path;
     ScannedTokens = {};
     States = {};
 }
 
-Scanner::Scanner(string p) : path(p), file(FileHandler(p)), current(0) {
+Scanner::Scanner(string p) : path(p), file(FileHandler(p)), current(0)
+{
     ScannedTokens = {};
     States = {};
 }
 
-int Scanner::popState() {
-    if (States.size() == 0) 
+int Scanner::popState()
+{
+    if (States.size() == 0)
         return -5;
-    
+
     int state = States.at(States.size() - 1);
     States.pop_back();
     return state;
 }
 
-bool Scanner::isAccepting(int state) {
-    for (int i = 0; i < acceptingStates.size(); i++) {
-        if (state == acceptingStates.at(i)) 
+bool Scanner::isAccepting(int state)
+{
+    for (int i = 0; i < acceptingStates.size(); i++)
+    {
+        if (state == acceptingStates.at(i))
             return true;
     }
     return false;
 }
 
-void Scanner::displayTokens() {
+void Scanner::displayTokens()
+{
     cout << "Format: <lexeme,category>" << endl;
-    for (auto tn : Scanner::ScannedTokens) {
+    for (auto tn : Scanner::ScannedTokens)
+    {
         cout << tn.to_str() << endl;
     }
 }
 
-int Scanner::getTransitionTableRow(char c) {
+int Scanner::getTransitionTableRow(char c)
+{
     if ((c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F'))
         return 0;
-    else if ((c >= 'g' && c <= 'z') || (c>= 'G' && c <= 'Z'))
+    else if ((c >= 'g' && c <= 'z') || (c >= 'G' && c <= 'Z'))
         return 1;
     else if (isdigit(c))
         return 2;
     else if (c == '(' || c == ')' || c == '{' || c == '}' || c == ';' || c == ':' || c == ',')
         return 15;
 
-    switch(c) {
-        case '+':
-            return 3;
-        case '-':
-            return 4;
-        case '<':
-            return 5;
-        case '>':
-            return 6;
-        case '#':
-            return 7;
-        case '/':
-            return 8;
-        case '!':
-            return 9;
-        case '=':
-            return 10;
-        case '*':
-            return 11;
-        case '_':
-            return 12;
-        case '.':
-            return 13;
-        case '\n': case ' ': case '\t':
-            return 14;
+    switch (c)
+    {
+    case '+':
+        return 3;
+    case '-':
+        return 4;
+    case '<':
+        return 5;
+    case '>':
+        return 6;
+    case '#':
+        return 7;
+    case '/':
+        return 8;
+    case '!':
+        return 9;
+    case '=':
+        return 10;
+    case '*':
+        return 11;
+    case '_':
+        return 12;
+    case '.':
+        return 13;
+    case '\n':
+    case ' ':
+    case '\t':
+        return 14;
     }
 }
 
-token Scanner::handleKeyword(string word) {
+token Scanner::handleKeyword(string word)
+{
     int i;
     int check = -1;
-    for (i = 0; i < keywords.size(); i++) {
-        if (word == keywords.at(i)) {
+    for (i = 0; i < keywords.size(); i++)
+    {
+        if (word == keywords.at(i))
+        {
             check = i;
             break;
         }
     }
 
-    //checking for 2 underscores
+    // checking for 2 underscores
     if (word.compare(0, 2, "__") == 0 && (i < 16))
-        //regular identifiers cannot start with an underscore
+        // regular identifiers cannot start with an underscore
         return token(word, INVALID_TOKEN);
-    
+
     if (check == -1)
         return token(word, IDENTIFIER);
-    
+
     return token(word, KeyToToken.at(word));
 }
 
-token Scanner::handleNumbers(string word) {
-    for (int i = 0; i < word.length(); i++) {
+token Scanner::handleNumbers(string word)
+{
+    for (int i = 0; i < word.length(); i++)
+    {
         if (word.at(i) == '.' && i < word.length() - 1)
-            return token(word,FLOAT_LIT);
+            return token(word, FLOAT_LIT);
     }
 
-    return token(word,INT_LIT);
+    return token(word, INT_LIT);
 }
 
-token Scanner::handlePunctuation(string word) {
-    return token(word,PuncToToken.at(word));
+token Scanner::handlePunctuation(string word)
+{
+    return token(word, PuncToToken.at(word));
 }
 
-token Scanner::handleOperator(string word) {
+token Scanner::handleOperator(string word)
+{
     return token(word, OpToToken.at(word));
 }
 
-token Scanner::handleRelationalOperator(string word) {
+token Scanner::handleRelationalOperator(string word)
+{
     return token(word, RelToToken.at(word));
 }
 
-void Scanner::getCategoryFromState(int state, string word) {
-    switch(state) {
-        case 0:
-            ScannedTokens.push_back(token()); 
-            break;
-        case 1:
-            ScannedTokens.push_back(handleKeyword(word));
-            break;
-        case 2:
-            ScannedTokens.push_back(handleNumbers(word));
-            break;
-        case 3:
-            ScannedTokens.push_back(token(word, INVALID_TOKEN));
-            break;
-        case 4: case 5: case 6: case 7: case 8: case 9:
-            ScannedTokens.push_back(token(word,INVALID_TOKEN));
-            break;
-        case 10:
-            ScannedTokens.push_back(token(word,COL_LIT));
-            break;
-        case 11:
-            ScannedTokens.push_back(handleOperator(word));
-            break;
-        case 12: case 13:
-            ScannedTokens.push_back(handleRelationalOperator(word));
-            break;
-        case 14:
-            cout << "Invalid Syntax: Incorrect use of \"!\"" << endl;
-            ScannedTokens.push_back(token(word,INVALID_TOKEN));
-            break;
-        case 15:
-            ScannedTokens.push_back(token(word,OP_REL_NOT_EQUAL)); //! CHANGE THE HANDLE RELATIONAL OPERATORS FUNCTION
-            break;
-        case 16:
-            ScannedTokens.push_back(token(word,OP_MUL_DIV));
-            break;
-        case 17: case 18: case 22: case 26:
-            break; //comments are ignored
-        case 19: case 20: case 21:
-            cout << "Invalid Syntax: Incomplete comment" << word << endl;
-            ScannedTokens.push_back(token(word,INVALID_TOKEN));
-            break;
-        case 23:
-            ScannedTokens.push_back(handlePunctuation(word));
-            break;
-        case 24:
-            ScannedTokens.push_back(token(word,OP_ADD_SUB));
-            break;
-        case 25:
-            ScannedTokens.push_back(token(word,OP_RET_TYPE));
-            break;
+void Scanner::getCategoryFromState(int state, string word)
+{
+    switch (state)
+    {
+    case 0:
+        ScannedTokens.push_back(token());
+        break;
+    case 1:
+        ScannedTokens.push_back(handleKeyword(word));
+        break;
+    case 2:
+        ScannedTokens.push_back(handleNumbers(word));
+        break;
+    case 3:
+        ScannedTokens.push_back(token(word, INVALID_TOKEN));
+        break;
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+        ScannedTokens.push_back(token(word, INVALID_TOKEN));
+        break;
+    case 10:
+        ScannedTokens.push_back(token(word, COL_LIT));
+        break;
+    case 11:
+        ScannedTokens.push_back(handleOperator(word));
+        break;
+    case 12:
+    case 13:
+        ScannedTokens.push_back(handleRelationalOperator(word));
+        break;
+    case 14:
+        cout << "Invalid Syntax: Incorrect use of \"!\"" << endl;
+        ScannedTokens.push_back(token(word, INVALID_TOKEN));
+        break;
+    case 15:
+        ScannedTokens.push_back(token(word, OP_REL_NOT_EQUAL)); //! CHANGE THE HANDLE RELATIONAL OPERATORS FUNCTION
+        break;
+    case 16:
+        ScannedTokens.push_back(token(word, OP_MUL_DIV));
+        break;
+    case 17:
+    case 18:
+    case 22:
+    case 26:
+        break; // comments are ignored
+    case 19:
+    case 20:
+    case 21:
+        cout << "Invalid Syntax: Incomplete comment" << word << endl;
+        ScannedTokens.push_back(token(word, INVALID_TOKEN));
+        break;
+    case 23:
+        ScannedTokens.push_back(handlePunctuation(word));
+        break;
+    case 24:
+        ScannedTokens.push_back(token(word, OP_ADD_SUB));
+        break;
+    case 25:
+        ScannedTokens.push_back(token(word, OP_RET_TYPE));
+        break;
     }
 }
 
-void Scanner::scanInput() {
-    while (this->file.getCurrPos() < this->file.getSizeOfProgram()) {
-        //initializing
+void Scanner::scanInput()
+{
+    while (this->file.getCurrPos() < this->file.getSizeOfProgram())
+    {
+        // initializing
         int state = 0;
         string word = "";
-        //used instead of a stack
+        // used instead of a stack
         this->States = {};
         States.push_back(-2);
 
-        while(state != -1) {
+        while (state != -1)
+        {
             char c = this->file.NextChar();
             word += c;
-            if (isAccepting(state)) {
+            if (isAccepting(state))
+            {
                 this->States = {};
                 States.push_back(-2);
             }
@@ -192,9 +228,11 @@ void Scanner::scanInput() {
             state = transition_table[row][state];
         }
 
-        while (!isAccepting(state) && state != -2) {
+        while (!isAccepting(state) && state != -2)
+        {
             state = popState();
-            if (state != -2) {
+            if (state != -2)
+            {
                 word.pop_back();
                 this->file.RollBack();
             }
@@ -204,17 +242,29 @@ void Scanner::scanInput() {
     }
 }
 
-token Scanner::peek() {
+token Scanner::peekNextToken()
+{
     return ScannedTokens.at(current);
 }
 
-token Scanner::getNextToken() {
+token Scanner::getNextToken()
+{
     token t;
-    try {
+    try
+    {
         t = ScannedTokens.at(current);
-    } catch(std::out_of_range) {
+    }
+    catch (std::out_of_range)
+    {
         return ScannedTokens.at(ScannedTokens.size() - 1);
     }
     ++current;
     return t;
+}
+
+token Scanner::rollBackToken()
+{
+    if (current > 0)
+        --current;
+    return ScannedTokens.at(current);
 }
--- Scanner/Scanner.h
+++ Scanner/Scanner.h
@@ -13,96 +13,88 @@ const vector<string> keywords =
      "let", "return", "fun", "__print", "__delay", "__pixelr", "__pixel", "__width", "__height", "__randi", "__read"};
 
 const vector<string> punctuation =
-{"(", ")", "{", "}", ";", ":", ","};
+    {"(", ")", "{", "}", ";", ":", ","};
 
-const vector<string> operators = 
-{"+","-","*","/"};
+const vector<string> operators =
+    {"+", "-", "*", "/"};
 
 const vector<string> relational_operators =
-{"=","<",">","==","<=",">=","!="};
+    {"=", "<", ">", "==", "<=", ">=", "!="};
 
 // -1 is an error state
 const vector<vector<int>> transition_table =
-{//                            0    1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26
-        /* a-f */           {  1,   1, -1, -1,  5,  6,  7,  8,  9, 10,  -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
-        /* g-z */           {  1,   1, -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
-        /* 0-9 */           {  2,   1,  2,  2,  5,  6,  7,  8,  9, 10,  -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
-        /* + */             { 11,  -1, -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
-        /* - */             { 24,  -1, -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
-        /* < */             { 12,  -1, -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
-        /* > */             { 12,  -1, -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, 25, -1, -1},
-        /* # */             {  4,  -1, -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1}, 
-        /* / */             { 16,  -1, -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, 17, 17, -1, 19, -1, 22, -1, -1, -1, -1, -1},
-        /* ! */             { 14,  -1, -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
-        /* = */             { 12,  -1, -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, 13, -1, 15, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
-        /* * */             { 11,  -1, -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, 19, 17, -1, 19, 21, 19, -1, -1, -1, -1, -1},
-        /* _ */             {  1,   1, -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
-        /* . */             { -1,  -1,  3, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
-        /* \n,space,\t */   { 26,  -1, -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, 18, -1, 20, -1, 19, -1, -1, -1, -1, -1},
-        /* (,),{,},;,:,, */ { 23,  -1, -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1}
-};
+    { //                            0    1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26
+        /* a-f */ {1, 1, -1, -1, 5, 6, 7, 8, 9, 10, -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
+        /* g-z */ {1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
+        /* 0-9 */ {2, 1, 2, 2, 5, 6, 7, 8, 9, 10, -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
+        /* + */ {11, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
+        /* - */ {24, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
+        /* < */ {12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
+        /* > */ {12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, 25, -1, -1},
+        /* # */ {4, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
+        /* / */ {16, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 17, 17, -1, 19, -1, 22, -1, -1, -1, -1, -1},
+        /* ! */ {14, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
+        /* = */ {12, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 13, -1, 15, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
+        /* * */ {11, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 19, 17, -1, 19, 21, 19, -1, -1, -1, -1, -1},
+        /* _ */ {1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
+        /* . */ {-1, -1, 3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1},
+        /* \n,space,\t */ {26, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 18, -1, 20, -1, 19, -1, -1, -1, -1, -1},
+        /* (,),{,},;,:,, */ {23, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 17, -1, 19, -1, 19, -1, -1, -1, -1, -1}};
 
-const vector<int> acceptingStates {1, 2, 10, 11, 12, 13, 15, 16, 17, 18, 22, 23, 24, 25, 26};
+const vector<int> acceptingStates{1, 2, 10, 11, 12, 13, 15, 16, 17, 18, 22, 23, 24, 25, 26};
 
-const map<string,TokenName> KeyToToken
+const map<string, TokenName> KeyToToken{
+    {"if", KEY_IF},
+    {"else", KEY_ELSE},
+    {"for", KEY_FOR},
+    {"while", KEY_WHILE},
+    {"bool", KEY_T_BOOL},
+    {"int", KEY_T_INT},
+    {"float", KEY_T_FLOAT},
+    {"colour", KEY_T_COLOUR},
+    {"true", KEY_BOOL_LIT_T},
+    {"false", KEY_BOOL_LIT_F},
+    {"and", OP_MUL_AND},
+    {"or", OP_ADD_OR},
+    {"not", OP_UNARY_NOT},
+    {"let", KEY_VAR_DEC},
+    {"return", KEY_RETURN},
+    {"fun", KEY_FUN_DEC},
+    {"__print", KEY_PRINT},
+    {"__delay", KEY_DELAY},
+    {"__pixelr", KEY_PIX_R},
+    {"__pixel", KEY_PIX},
+    {"__width", KEY_PAD_W},
+    {"__height", KEY_PAD_H},
+    {"__randi", KEY_PAD_RANDI},
+    {"__read", KEY_PAD_READ}};
+
+const map<string, TokenName> PuncToToken{
+    {"(", PUNCT_OPEN_PAR},
+    {")", PUNCT_CLOSED_PAR},
+    {"{", PUNCT_OPEN_CURL},
+    {"}", PUNCT_CLOSED_CURL},
+    {";", PUNCT_SEMICOLON},
+    {":", PUNCT_COLON},
+    {",", PUNCT_COMMA}};
+
+const map<string, TokenName> OpToToken{
+    {"+", OP_ADD_ADD},
+    {"-", OP_ADD_SUB},
+    {"*", OP_MUL_MUL},
+    {"/", OP_MUL_DIV}};
+
+const map<string, TokenName> RelToToken{
+    {"=", OP_ASSIGNMENT},
+    {"<", OP_REL_LESS},
+    {">", OP_REL_GREAT},
+    {"==", OP_REL_EQUAL},
+    {"<=", OP_REL_LESS_EQ},
+    {">=", OP_REL_GREAT_EQ},
+    {"!=", OP_REL_NOT_EQUAL}};
+
+class Scanner
 {
-        {"if",KEY_IF},
-        {"else",KEY_ELSE},
-        {"for",KEY_FOR},
-        {"while",KEY_WHILE},
-        {"bool",KEY_T_BOOL},
-        {"int",KEY_T_INT},
-        {"float",KEY_T_FLOAT},
-        {"colour",KEY_T_COLOUR},
-        {"true",KEY_BOOL_LIT_T},
-        {"false",KEY_BOOL_LIT_F},
-        {"and",OP_MUL_AND},
-        {"or",OP_ADD_OR},
-        {"not",OP_UNARY_NOT},
-        {"let",KEY_VAR_DEC},
-        {"return",KEY_RETURN},
-        {"fun",KEY_FUN_DEC},
-        {"__print",KEY_PRINT},
-        {"__delay",KEY_DELAY},
-        {"__pixelr",KEY_PIX_R},
-        {"__pixel",KEY_PIX},
-        {"__width",KEY_PAD_W},
-        {"__height",KEY_PAD_H},
-        {"__randi",KEY_PAD_RANDI},
-        {"__read",KEY_PAD_READ}
-};
-
-const map<string,TokenName> PuncToToken
-{
-        {"(",PUNCT_OPEN_PAR},
-        {")",PUNCT_CLOSED_PAR},
-        {"{",PUNCT_OPEN_CURL},
-        {"}",PUNCT_CLOSED_CURL},
-        {";",PUNCT_SEMICOLON},
-        {":",PUNCT_COLON},
-        {",",PUNCT_COMMA}
-};
-
-const map<string,TokenName> OpToToken
-{
-        {"+",OP_ADD_ADD},
-        {"-",OP_ADD_SUB},
-        {"*",OP_MUL_MUL},
-        {"/",OP_MUL_DIV}        
-};
-
-const map<string,TokenName> RelToToken
-{
-        {"=",OP_ASSIGNMENT},
-        {"<",OP_REL_LESS},
-        {">",OP_REL_GREAT},
-        {"==",OP_REL_EQUAL},
-        {"<=",OP_REL_LESS_EQ},
-        {">=",OP_REL_GREAT_EQ},
-        {"!=",OP_REL_NOT_EQUAL}
-};
-
-class Scanner {
 private:
         string path;
         vector<token> ScannedTokens;
@@ -120,13 +112,15 @@ private:
         token handleOperator(string word);
         token handleRelationalOperator(string word);
         void getCategoryFromState(int state, string word);
+
 public:
-        //constructors
+        // constructors
         Scanner();
         Scanner(string p);
 
-        void displayTokens();        
+        void displayTokens();
         void scanInput();
-        token peek();
         token getNextToken();
+        token peekNextToken();
+        token rollBackToken();
 };
