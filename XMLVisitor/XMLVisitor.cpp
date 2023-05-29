d570cee0a22e355d8e768f21bf5a1a11c15167b3
DamFil <damjan.filipovic.21@um.edu.mt>
2023-05-29 21:29:14 +0200

    Finished implementing the XMLVisitor class

--- XMLVisitor/XMLVisitor.cpp
+++ XMLVisitor/XMLVisitor.cpp
@@ -274,3 +274,176 @@ void XMLVisitor::visit(ASTStatement *s)
         cout << "Error generating XML Code: Problem with the Statement node" << endl;
     }
 }
+
+void XMLVisitor::visit(ASTFunCall *fncall)
+{
+    printIndent();
+    cout << "<FunCall>" << endl;
+    ++indent;
+    fncall->id->accept(this);
+    fncall->params->accept(this);
+    --indent;
+    printIndent();
+    cout << "</FunCall>" << endl;
+}
+
+void XMLVisitor::visit(ASTFunDec *fndec)
+{
+    printIndent();
+    cout << "<FunDec type=\"" << fndec->rtrntype << "\">>" << endl;
+    ++indent;
+    fndec->id->accept(this);
+    fndec->params->accept(this);
+    fndec->block->accept(this);
+    --indent;
+    printIndent();
+    cout << "</FunDec>" << endl;
+}
+
+void XMLVisitor::visit(ASTRtrnStmnt *rtrn)
+{
+    printIndent();
+    cout << "<Return>" << endl;
+    ++indent;
+    rtrn->expr->accept(this);
+    --indent;
+    printIndent();
+    cout << "</Return>" << endl;
+}
+
+void XMLVisitor::visit(ASTParams *params)
+{
+    printIndent();
+    cout << "<ActualParameters>" << endl;
+    ++indent;
+    for (auto i : params->expressions)
+    {
+        i->accept(this);
+    }
+    --indent;
+    printIndent();
+    cout << "</ActualParameters>" << endl;
+}
+
+void XMLVisitor::visit(ASTFormalParam *fparam)
+{
+    printIndent();
+    cout << "<Param type=\"" << fparam->type << "\">" << endl;
+    ++indent;
+    fparam->id->accept(this);
+    --indent;
+    printIndent();
+    cout << "</Param>" << endl;
+}
+
+void XMLVisitor::visit(ASTFormalParams *fparams)
+{
+    printIndent();
+    cout << "<FormalParameters>" << endl;
+    ++indent;
+    for (auto i : fparams->params)
+    {
+        i->accept(this);
+    }
+    --indent;
+    printIndent();
+    cout << "</FormalParameters>" << endl;
+}
+
+void XMLVisitor::visit(ASTIfStmn *ifstmn)
+{
+    printIndent();
+    cout << "<IfStmnt>" << endl;
+    ++indent;
+    ifstmn->cond->accept(this);
+    ifstmn->ifbody->accept(this);
+    if (ifstmn->elsebody != nullptr)
+        ifstmn->elsebody->accept(this);
+    --indent;
+    printIndent();
+    cout << "</IfStmnt>" << endl;
+}
+
+void XMLVisitor::visit(ASTIfBody *ifbody)
+{
+    printIndent();
+    cout << "<IfBody>" << endl;
+    ++indent;
+    for (int i = 0; i < ifbody->stmnts.size(); i++)
+    {
+        ifbody->stmnts.at(i)->accept(this);
+    }
+    --indent;
+    printIndent();
+    cout << "</IfBody>" << endl;
+}
+
+void XMLVisitor::visit(ASTElseBody *elsebody)
+{
+    printIndent();
+    cout << "<ElseBody>" << endl;
+    ++indent;
+    for (auto i : elsebody->stmnts)
+    {
+        i->accept(this);
+    }
+    --indent;
+    printIndent();
+    cout << "</ElseBody>" << endl;
+}
+
+void XMLVisitor::visit(ASTWhile *whilestmnt)
+{
+    printIndent();
+    cout << "<While>" << endl;
+    ++indent;
+    whilestmnt->condtn->accept(this);
+    whilestmnt->stmnts->accept(this);
+    --indent;
+    printIndent();
+    cout << "</While>" << endl;
+}
+
+void XMLVisitor::visit(ASTFor *forstmnt)
+{
+    printIndent();
+    cout << "<For>" << endl;
+    ++indent;
+    if (forstmnt->vardec != nullptr)
+        forstmnt->vardec->accept(this);
+    forstmnt->expr->accept(this);
+    if (forstmnt->assignment != nullptr)
+        forstmnt->assignment->accept(this);
+    forstmnt->block->accept(this);
+    --indent;
+    printIndent();
+    cout << "</For>" << endl;
+}
+
+void XMLVisitor::visit(ASTBlock *block)
+{
+    printIndent();
+    cout << "<Block>" << endl;
+    ++indent;
+    for (auto i : block->stmnts)
+    {
+        i->accept(this);
+    }
+    --indent;
+    printIndent();
+    cout << "</Block>" << endl;
+}
+
+void XMLVisitor::visit(ASTProgram *program)
+{
+    printIndent();
+    cout << "<Program>" << endl;
+    ++indent;
+    for (auto i : program->stmnts)
+    {
+        i->accept(this);
+    }
+    --indent;
+    printIndent();
+    cout << "</Program>" << endl;
+}
