105c513dc0a4e07f6e5999ffc1e0751b5408425c
DamFil <93907223+DamFil@users.noreply.github.com>
2023-06-09 23:36:23 +0200

    Modified tests directory

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -3,20 +3,26 @@
 #include "../XMLVisitor/XMLVisitor.h"
 #include "../SemanticAnalysis/SemVisitor.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc == 1)
+    {
+        printf("\nYou need to specify the absolute path of the text file you wish to compile");
+        return -1;
+    }
 
-    Scanner *lexer = new Scanner("C:/Users/dakif/OneDrive/UniCode/CompilerTheory/PixArLang-Compiler/Tests/test.txt");
-    Scanner *lexer2 = new Scanner("C:/Users/dakif/OneDrive/UniCode/CompilerTheory/PixArLang-Compiler/Tests/test.txt");
-    Scanner *lexer3 = new Scanner("C:/Users/dakif/OneDrive/UniCode/CompilerTheory/PixArLang-Compiler/Tests/test.txt");
-    Scanner *lexer4 = new Scanner("C:/Users/dakif/OneDrive/UniCode/CompilerTheory/PixArLang-Compiler/Tests/test.txt");
-    Scanner *lexer5 = new Scanner("C:/Users/dakif/OneDrive/UniCode/CompilerTheory/PixArLang-Compiler/Tests/test.txt");
+
+
+    Scanner *lexer = new Scanner(argv[1]);
+    //Scanner *lexer2 = new Scanner("C:/Users/dakif/OneDrive/UniCode/CompilerTheory/PixArLang-Compiler/Tests/test2.txt");
+    //Scanner *lexer3 = new Scanner("C:/Users/dakif/OneDrive/UniCode/CompilerTheory/PixArLang-Compiler/Tests/test3.txt");
+    //Scanner *lexer4 = new Scanner("C:/Users/dakif/OneDrive/UniCode/CompilerTheory/PixArLang-Compiler/Tests/test4.txt");
 
     lexer->scanInput();
-    lexer2->scanInput();
-    lexer3->scanInput();
-    lexer4->scanInput();
-    lexer5->scanInput();
+    lexer->displayTokens();
+    //lexer2->scanInput();
+    //lexer3->scanInput();
+    //lexer4->scanInput();
 
     XMLVisitor *visitor = new XMLVisitor();
     SemanticVisitor *semvisitor = new SemanticVisitor();
@@ -26,27 +32,21 @@ int main()
     cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~PROGRAM 1~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
     visitor->visit(parser.root);
 
-    Parser parser2 = Parser(lexer2);
-    parser2.parse();
-    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~PROGRAM 2~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-    visitor->visit(parser2.root);
+    //Parser parser2 = Parser(lexer2);
+    //parser2.parse();
+    //cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~PROGRAM 2~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    //visitor->visit(parser2.root);
+//
+    //Parser parser3 = Parser(lexer3);
+    //parser3.parse();
+    //cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~PROGRAM 3~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    //visitor->visit(parser3.root);
+//
+    //Parser parser4 = Parser(lexer4);
+    //parser4.parse();
+    //cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~PROGRAM 4~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    //visitor->visit(parser4.root);
 
-    Parser parser3 = Parser(lexer3);
-    parser3.parse();
-    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~PROGRAM 3~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-    visitor->visit(parser3.root);
-
-    Parser parser4 = Parser(lexer4);
-    parser4.parse();
-    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~PROGRAM 4~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-    visitor->visit(parser4.root);
-
-    Parser parser5 = Parser(lexer5);
-    parser5.parse();
-    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~PROGRAM 5~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-    visitor->visit(parser5.root);
-
-    //semvisitor->visit(parser.root);
     //semvisitor->visit(parser2.root);
     //semvisitor->visit(parser3.root);
     //semvisitor->visit(parser4.root);
